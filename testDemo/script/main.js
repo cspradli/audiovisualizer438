48162a2d1b6b766a2b7d5f754b3643a20699a0f0
Caleb Spradlin <cspradli@unca.edu>
2019-04-05 15:08:04 -0400

    added test stuff

--- testDemo/script/main.js
+++ testDemo/script/main.js
@@ -1,14 +1,42 @@
 // init
 
+var world = tQuery.createWorld().boilerplate().start();
 
 function initAudio(){
-    var webaudio = new webaudio();
-
+    var webaudio = new WebAudio();
+    /*
     var sound = webaudio.createSound().load('audio/africa.mp3', function(sound){
         sound.loop(true).play();
-    });
+    });*/
 }
 
 function initWorld(){
-    var world = tQuery.createWorld().boilerplate().start();
+    //var object = tQuery.createTorus().addTo(world);
+    //world.renderer().setClearColorHex(0x000000, world.renderer().getClearAlpha() );
+    tQuery.createAmbientLight().addTo(world).color(0x888888);
+    tQuery.createDirectionalLight().addTo(world).position(+1,+1,1).color(0x88FF88);
+    tQuery.createDirectionalLight().addTo(world).position(-1,-1, 1).color(0x8888FF);
+
+}
+
+function createVueMeter(){
+    var nBar = 41;
+    var barW = 80/nBar;
+    var bars3d = [];
+    var group3d = tQuery.createObject3D().scale(1/20).addTo(world);
+
+    for (var i=0; i <nBar; i++){
+        var bar3d = tQuery.createCube(barW, 10, 5, new THREE.MeshLambertMaterial({
+            ambient: 0x888888,
+            color: 0xffffff
+        }));
+        bar3d.addTo(group3d).position((i-nBar/2)*barW, 0, 0);
+        bars3d.push(bar3d);
+    }
+}
+
+function mainRun(){
+    initAudio();
+    initWorld();
+    createVueMeter();
 }
