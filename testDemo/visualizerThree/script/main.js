7d7a5eee61501b07c31b85b295576a53479b3a16
Caleb Spradlin <cspradli@unca.edu>
2019-04-09 16:27:44 -0400

    added audio

--- testDemo/visualizerThree/script/main.js
+++ testDemo/visualizerThree/script/main.js
@@ -1,3 +1,11 @@
+var simplexNoise = new simplexNoise();
+
 var visualInit = function(){
-    
+    var audio = document.getElementById("audio");
+
+    document.onload = function(e){
+        console.log(e);
+        audio.play();
+        play();
+    }
 }
