fb986297ef19e40971692d113e4db5289fed4bfe
Caleb Spradlin <cspradli@unca.edu>
2019-04-09 16:36:17 -0400

    added scene in sceneSetup()

--- testDemo/visualizerThree/script/main.js
+++ testDemo/visualizerThree/script/main.js
@@ -6,12 +6,33 @@ var visualInit = function(){
     document.onload = function(e){
         console.log(e);
         audio.play();
-        play();
+        audioSetup();
     }
 
     audio.src="../sounds";
     audio.load();
     audio.play();
-    play();
+    audioSetup();
+}
 
+function audioSetup(){
+
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    analyser.fftSize = 512;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    sceneSetup();
+}
+
+function sceneSetup(){
+    var scene = new Three.Scene();
+    var group = new Three.Group();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0,0,100);
+    camera.lookAt(scene.position);
+    scene.add(camera);
 }
